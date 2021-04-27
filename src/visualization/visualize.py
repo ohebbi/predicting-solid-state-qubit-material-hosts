572ced38fc96da75f3fbb86c596a53789fd16741
Oliver Hebnes <oliver.lerstol.hebnes@gyldendal.no>
2021-04-27 17:35:33 +0200

    fig

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -547,8 +547,8 @@ def plot_important_features(models, X, k, n, prettyNames, numPC, approach, numFe
     ax.set_xlabel("Principal components")
     ax.legend(loc="upper right")
     tikzplotlib.save(dir_path / Path(approach + "PC" + ".tex"),
-                                axis_height = str(set_size(width, 0.8, isTex=True)[0]) + "in",
-                                axis_width  = str(set_size(width, 0.8, isTex=True)[0]) + "in")
+                                axis_height = str(set_size(width, 0.4, isTex=True)[0]) + "in",
+                                axis_width  = str(set_size(width, 1.0, isTex=True)[0]) + "in")
     fig.show()
     #plt.title('Feature Importances')
     #plt.barh(importances, range(len(indices)), color='b', align='center')
